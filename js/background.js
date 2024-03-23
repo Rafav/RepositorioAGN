ced9967f5c2e437804f91451dbe0fa462a5db17e
Rafav <esi.medina@gmail.com>
2024-03-23 12:28:42 +0100

    Timeout entre descargas

--- js/background.js
+++ js/background.js
@@ -3,16 +3,24 @@
 const url_base='https://repositorio.agn.gob.mx/api/img/image/';
 
 
+
+function delay(time) {
+    return new Promise(resolve => setTimeout(resolve, time));
+}
+
+
 async function descargarYProcesarJSON(url) {
     try {
         let response = await fetch(url); // Descargar el JSON
         let data = await response.json(); // Convertir a objeto JavaScript
 
         // Asumiendo que el JSON es un arreglo de objetos:
+
         for (let item of data) {
             if ('idImagenDigitalizada' in item) {
                 console.log(url_base+item.idImagenDigitalizada); // Procesa cada idImagenDigitalizada
                 chrome.downloads.download({ url: url_base+item.idImagenDigitalizada});
+                await delay(5000)
 
             }
         }
